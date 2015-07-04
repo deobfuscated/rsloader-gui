719157c50af088aeece88131569f408f2154724e
Yongyi Chen <yongyi781@hotmail.com>
2015-07-04 03:34:51 -0400

    Removed extra empty lines

--- rsloader-gui/src/rsloader/GameStub.java
+++ rsloader-gui/src/rsloader/GameStub.java
@@ -6,46 +6,44 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class GameStub implements AppletStub {
+    private GameParameters params;
 
-	private GameParameters params;
-	
-	public GameStub(GameParameters params) {
-		this.params = params;
-	}
-	
-	@Override
-	public boolean isActive() {
-		return false;
-	}
+    public GameStub(GameParameters params) {
+        this.params = params;
+    }
 
-	@Override
-	public URL getDocumentBase() {
-		return getCodeBase();
-	}
+    @Override
+    public boolean isActive() {
+        return false;
+    }
 
-	@Override
-	public URL getCodeBase() {
-		try {
-			return new URL(params.getCodeBase());
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-			System.exit(0);
-			return null;
-		}
-	}
+    @Override
+    public URL getDocumentBase() {
+        return getCodeBase();
+    }
 
-	@Override
-	public String getParameter(String name) {
-		return params.getParameter(name);
-	}
+    @Override
+    public URL getCodeBase() {
+        try {
+            return new URL(params.getCodeBase());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        return null;
+    }
 
-	@Override
-	public AppletContext getAppletContext() {
-		return null;
-	}
+    @Override
+    public String getParameter(String name) {
+        return params.getParameter(name);
+    }
 
-	@Override
-	public void appletResize(int width, int height) {
-	}
-	
+    @Override
+    public AppletContext getAppletContext() {
+        return null;
+    }
+
+    @Override
+    public void appletResize(int width, int height) {
+    }
 }
